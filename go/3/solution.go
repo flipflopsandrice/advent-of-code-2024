2adaf2a722b01bd6cdca24bab504df0560691b75
Erik Beijerman <erik@bmntech.io>
2024-12-03 14:13:15 +0100

    ts & go 3 part 2

--- go/3/solution.go
+++ go/3/solution.go
@@ -10,7 +10,6 @@ func solution(input string) int {
 	regex := regexp.MustCompile("mul\\(([0-9+]{0,}),([0-9+]{0,})\\)")
 	matches := regex.FindAllStringSubmatch(input, -2)
 
-	fmt.Println(matches)
 	var total int = 0
 	for _, match := range matches {
 		multiplier, err := strconv.Atoi(match[1])
@@ -25,6 +24,34 @@ func solution(input string) int {
 		}
 
 		total += multiplier * value
+	}
+	return total
+}
+
+func part2(input string) int {
+	regex := regexp.MustCompile("(do|don\\'t|mul)\\((([0-9+]{0,}),([0-9+]{0,}))?\\)")
+	matches := regex.FindAllStringSubmatch(input, -2)
+
+	var total int = 0
+	active := true
+	for _, match := range matches {
+		function := match[1]
+		if function == "mul" && active {
+			multiplier, err := strconv.Atoi(match[3])
+			if err != nil {
+				fmt.Println("Error parsing multiplier")
+				return -1
+			}
+			value, err := strconv.Atoi(match[4])
+			if err != nil {
+				fmt.Println("Error parsing value")
+				return -1
+			}
+
+			total += multiplier * value
+		} else {
+			active = function == "do"
+		}
 	}
 	return total
 }
--- ts/3/solution.ts
+++ ts/3/solution.ts
@@ -3,9 +3,26 @@ export const solution = (input: string) => {
 
   let total = 0;
   (matches ?? []).forEach((match) => {
-    const [,a, b] = match;
+    const [, a, b] = match;
     total += parseInt(a) * parseInt(b);
   });
 
-    return total;
+  return total;
+};
+
+export const part2 = (input: string) => {
+  const matches = [...input.matchAll(/(do|don\'t|mul)\((([0-9+]{0,}),([0-9+]{0,}))?\)/gm)];
+
+  let total = 0;
+  let active = true;
+  (matches ?? []).forEach((match) => {
+    const [, func, , a, b] = match;
+    if (func === 'mul' && active) {
+      total += parseInt(a) * parseInt(b);
+    } else {
+      active = func === 'do'
+    }
+  });
+
+  return total;
 };
